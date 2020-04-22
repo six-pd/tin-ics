913c0b209395917d208b4a26b9e7d5bf58abc072
Krzysztof Jan Blankiewicz <k.blankiewicz@stud.elka.pw.edu.p>
2020-04-22 07:17:43 -0700

    obsluga bledow i zawiazywania polacznia w serwerze

--- server/src/cpp_src/Client.cpp
+++ server/src/cpp_src/Client.cpp
@@ -1,4 +1,5 @@
 #include "Client.h"
+#include <string.h>
 
 std::vector<Client*> Client::clientsList;
 
@@ -10,13 +11,69 @@ Client::Client(int newSocket)
 
 void Client::callProperMethod()
 {
-	int msgFlag = (buf[0] - '0')*10 + (buf[1] - '0');
-	std::cout << "ProperMethod: " << msgFlag << std::endl;
+	int msgFlag = getFlagFromMsg();
+	if(msgFlag == CL_CONNECTION_REQ)
+	{
+		startConnection();
+		return;
+	}
+	if(msgFlag == CL_END_REQ)
+	{
+		endConnection();
+		return;
+	}
+	if(msgFlag == CL_LIST_REQ)
+	{
+	sendClientsList();
+		return;
+	}
 }
 
 void Client::startConnection()
 {
+	std::string stringToSend;
 
+	// Server: 	SRV_CHALLENGE_REQ 
+	stringToSend = std::to_string(SRV_CHALLENGE_REQ);
+	sendString(stringToSend);
+ 	// Client: 	CL_CHALLENGE_RESP 
+	if(!receiveData() || getFlagFromMsg() != CL_CHALLENGE_RESP)
+	{
+		protocolError(CL_CHALLENGE_RESP);
+		return;
+	}
+ 	// Server: 	SRV_CHALLENGE_ACC 
+					// sparwdzic challenge
+	stringToSend = std::to_string(SRV_CHALLENGE_ACC);
+	sendString(stringToSend);
+ 	// Client: 	CL_SSID_REQ 
+	if(!receiveData() || getFlagFromMsg() != CL_SSID_REQ)
+	{
+		protocolError(CL_SSID_REQ);
+		return;
+	}
+ 	// Server: 	SRV_NEW_SSID 
+	if(getIntArg(1) == 0)
+	{
+		stringToSend = std::to_string(SRV_NEW_SSID);
+		sendString(stringToSend);
+	}
+	// Server: 	SRV_SSID_ACC
+	else
+	{
+		stringToSend = std::to_string(SRV_SSID_ACC);
+		sendString(stringToSend);
+	}
+ 	// Client: 	CL_NAME
+	if(!receiveData() || getFlagFromMsg() != CL_NAME)
+	{
+		protocolError(CL_NAME);
+		return;
+	}
+ 	// Server: 	SRV_NAME_ACC
+					// zapisz imie
+	stringToSend = std::to_string(SRV_CHALLENGE_ACC);
+	sendString(stringToSend);
 }
 
 void Client::sendClientsList()
@@ -29,6 +86,118 @@ void Client::endConnection()
 
 }
 
+void Client::sendString(std::string s)
+{
+	strcpy(bufOut, s.c_str());
+	write(mySocket, bufOut, 1024);
+}
+
+bool Client::receiveData()
+{
+	int rval;
+	memset(bufIn, 0, sizeof(bufIn));
+   	if ((rval = read(mySocket, bufIn, 1024)) == -1)
+    {
+		std::cout << "Error reading stream message" << std::endl;
+		return false;
+	}
+    else if (rval == 0)
+    {
+		std::cout << "Ending connection\n" << std::endl;
+		return false;
+	}
+	else if (rval > 1024)
+	{
+		std::cout << "Buffer overflow" << std::endl;
+		return false;
+	}
+	else
+		return true;
+}
+
+int Client::getFlagFromMsg()
+{
+	return getIntArg(0);
+}
+
+int Client::getIntArg(int argNum)
+{
+	int i = 0;
+	int currentArg = 0;
+	while(currentArg != argNum)
+	{
+		if(bufIn[i] == ';')
+			++currentArg;
+		++i;
+		if(i >= BUF_SIZE)
+		{
+			sequrityError();
+			return -1;
+		}
+	}
+	// teraz i wskazuje na pierwszy element szukanego argumentu
+	std::string targetArgument;
+	while(bufIn[i] != ';')
+	{
+		targetArgument += bufIn[i];
+		++i;
+		if(i >= BUF_SIZE)
+		{
+			sequrityError();
+			return -1;
+		}
+	}
+	// mamy szukany argument jako string
+	int result = 0;
+	for(i = 0; i < targetArgument.size(); ++i)
+	{
+		result = result*10 + (targetArgument[i] - '0');
+	}
+	return result;
+}
+
+std::string Client::getStringArg(int argNum)
+{
+	int i = 0;
+	int currentArg = 0;
+	while(currentArg != argNum)
+	{
+		if(bufIn[i] == ';')
+			++currentArg;
+		++i;
+		if(i >= BUF_SIZE)
+		{
+			sequrityError();
+			return "";
+		}
+	}
+	// teraz i wskazuje na pierwszy element szukanego argumentu
+	std::string targetArgument;
+	while(bufIn[i] != ';')
+	{
+		targetArgument += bufIn[i];
+		++i;
+		if(i >= BUF_SIZE)
+		{
+			sequrityError();
+			return "";
+		}
+	}
+	return targetArgument;
+}
+
+void Client::protocolError(int flagExpected)
+{
+	std::cout << "Protocol error, " << flagExpected << " with proper agruments expected. " << 
+			"Aborting sequence." << std::endl;
+}
+
+void Client::sequrityError()
+{
+	std::cout << "Wrong sent data - possible threat" << std::endl;
+	// zrobic klientowi jakas krzywde
+}
+
 void* Client::handleClient()
 {
 	std::cout << "In thread " << pthread_self() << std::endl;
@@ -36,15 +205,11 @@ void* Client::handleClient()
     int rval = 0;
 
     do{
-   		memset(buf, 0, sizeof(buf));
-   		if ((rval = read(mySocket,buf, 1024)) == -1)
-    		std::cout << "Error reading stream message" << std::endl;
-    	if (rval == 0)
-    		std::cout <<"Ending connection\n" << std::endl;
-    	else
-        	std::cout << "Otrzymana wiadomosc: " << buf << std::endl;
-		callProperMethod();
-	}while(rval != 0);
+		if(receiveData())
+			callProperMethod();
+		else
+			break;
+	}while(true);
 	close(mySocket);
 	// to jest turbo dziwne:
 	delete this;	
--- server/src/cpp_src/Client.h
+++ server/src/cpp_src/Client.h
@@ -3,25 +3,44 @@
 #include <netinet/in.h>
 #include <netdb.h>
 #include <unistd.h>
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 #include <stdio.h>
 #include <stdlib.h>
 
 #include <pthread.h>
 #include <vector>
+#include <string>
+#include "api.h"
 
 class Client
-{    
+{
     static std::vector<Client*> clientsList;
-    int mySocket;       // moj socket
-    char buf[1024];     // tu bedzie zapisywane co przyjdzie od klientat
+    int mySocket;           // moj socket
+    std::string name;
+    int ssid;
+    char bufIn[BUF_SIZE];       // te dwa nie sa tutaj konieczne,
+    char bufOut[BUF_SIZE];      // ale przydadza sie do obslugi bledow
+                                // BUF_SIZE jest na razie w api.h
+    
 
-    void callProperMethod();
-    void startConnection();
+    void callProperMethod();    // decyduje ktora metode wywolac w zaleznosci od wiadomosci klienta
+    void sendString(std::string s);
+    bool receiveData();     // zwraca true jesli nie ma zadnych bledow
+    int getFlagFromMsg();   // pobiera flage z bufIn, 
+                            // zakladam, ze flagi sa dwucyfrowe
+    int getIntArg(int argNum);  // zerowy argument to flaga
+    std::string getStringArg(int argNum);
+    void protocolError(int flagExpected);
+    void sequrityError();
+
+        // ponizsze trzy metody jak zostana wywolane, to przejmuja na chwile zarzadzenie sesja od handleClient
+        // czyli wymieniaja kilka wiadomosci z klientem i dopiero sie koncza
+    void startConnection();     
     void sendClientsList();
     void endConnection();
+
 public:
     Client(int newSocket);
-    void* handleClient();
+    void* handleClient();   // wywoluje sie na poczatku, czeka na dane, wola callProperMethod
 };
